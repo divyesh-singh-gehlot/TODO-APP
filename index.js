38a9dae22682b967dede89c161e657b815c144b2
DIVYESH SINGH GEHLOT <gehlotdivyeshsingh@gmail.com>
2025-05-31 17:03:13 +0530

    Added Add task and List Task feature

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const moment = require("moment")
 
 const connectionUrl = "mongodb://127.0.0.1:27017/todoDb"
 
@@ -10,39 +12,93 @@ mongoose
 
 const PORT = 5000;
 
+const todoSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+}, { timestamps: true })
+
+const Todo = mongoose.model("todo", todoSchema)
+
 //Initialize app
 const app = express();
 
 //Set view engine
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.get("/", (req, res)=>{
+
+//Setting up routes
+app.get("/", async (req, res) => {
     try {
-        res.render("index")
+
+        const tasks = await Todo.find({})
+
+        res.locals.moment = moment;
+
+        res.render("index", { title: "Home", tasks })
     } catch (error) {
         res.status(500).send(error.message)
     }
 });
 
-app.get("/add-task", (req, res)=>{
+app.get("/add-task", (req, res) => {
     try {
-        res.render("addTodo")
+        res.render("addTodo", { title: "Add" })
     } catch (error) {
         res.status(400).send(error.message)
     }
 });
 
-app.get("/update-task", (req, res)=>{
+app.get("/update-task", (req, res) => {
     try {
-        res.render("updateTodo")
+        res.render("updateTodo", { title: "Update" })
     } catch (error) {
         res.status(400).send(error.message)
     }
 });
 
-app.get("/delete-task", (req, res)=>{
+app.get("/delete-task", (req, res) => {
     try {
-        res.render("deleteTodo")
+        res.render("deleteTodo", { title: "Delete" })
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+});
+
+app.post("/add-task", async (req, res, next)=> {
+    try {
+        const {title, description} = req.body;
+
+        if(!title && !description){
+            res.status(400).send({message:"Both the Fields are Required"});
+        }
+        if(!title){
+            res.status(400).send({message:"Title is Required"});
+        }
+        if(!description){
+            res.status(400).send({message:"Description is Required"});
+        }
+        
+        const newTodo = new Todo({title, description});
+        await newTodo.save();
+
+        console.log("Title and description saved to DB.")
+
+        res.redirect("/")
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+app.post("/update-task", async (req, res) => {
+    try {
+        const {id} = req.query;
+        const {title, description} = req.body;
+
+        await Todo.findOneAndUpdate({id}, {title, description})
+        console.log("Data Updated Successfully!!!")
+
+        res.redirect("/")
     } catch (error) {
         res.status(400).send(error.message)
     }
